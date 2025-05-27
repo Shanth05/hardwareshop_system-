1f1646499e0b2f0a9934f5c2d3132246cfd853e0
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-05-27 23:42:22 +0530

    Update footer.php

--- hardware/components/footer.php
+++ hardware/components/footer.php
@@ -28,14 +28,14 @@
 
       <div class="box">
          <h3>Follow Us</h3>
-         <a href="https://www.facebook.com/harshchaudharynp" target="_blank"><i class="fab fa-facebook-f"></i>facebook</a>
-         <a href="https://twitter.com/HarshCh53587355" target="_blank"><i class="fab fa-twitter"></i>Twitter</a>
-         <a href="https://www.instagram.com/harshchy__/" target="_blank"><i class="fab fa-instagram"></i>Instagram</a>
-         <a href="https://www.linkedin.com/in/harsh-chaudhary-15763a150/" target="_blank"><i class="fab fa-linkedin"></i>Linkedin</a>
+         <a href="https://www.facebook.com/harshchaudharynp" target="_blank"><i class="fab fa-facebook-f"></i> Facebook</a>
+         <a href="https://twitter.com/HarshCh53587355" target="_blank"><i class="fab fa-twitter"></i> Twitter</a>
+         <a href="https://www.instagram.com/harshchy__/" target="_blank"><i class="fab fa-instagram"></i> Instagram</a>
+         <a href="https://www.linkedin.com/in/harsh-chaudhary-15763a150/" target="_blank"><i class="fab fa-linkedin"></i> Linkedin</a>
       </div>
 
    </section>
 
-   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span> K.N RAAM hardware | all rights reserved!</div>
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>K.N RAAM hardware</span> | all rights reserved!</div>
 
 </footer>
