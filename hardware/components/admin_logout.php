8f2276bd79cc521d50c91a37f91c87401e043aed
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-05-27 23:42:14 +0530

    Update admin_logout.php

--- hardware/components/admin_logout.php
+++ hardware/components/admin_logout.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 include 'connect.php';
 
@@ -6,6 +7,6 @@ session_start();
 session_unset();
 session_destroy();
 
-header('location:../admin/admin_login.php');
-
+header('Location: ../admin/admin_login.php');
+exit;
 ?>
