f8bec8d5956bfaafaaaf3094ff36ed6f413afb02
Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-05-29 23:57:38 +0530

    Update user_register.php

--- hardware/user_register.php
+++ hardware/user_register.php
@@ -1,34 +1,29 @@
 <?php
+declare(strict_types=1);
 
 include 'components/connect.php';
 
 session_start();
 
-if(isset($_SESSION['user_id'])){
-   $user_id = $_SESSION['user_id'];
-}else{
-   $user_id = '';
-};
+$user_id = $_SESSION['user_id'] ?? '';
 
 if(isset($_POST['submit'])){
 
-   $name = $_POST['name'];
-   $name = filter_var($name, FILTER_SANITIZE_STRING);
-   $email = $_POST['email'];
-   $email = filter_var($email, FILTER_SANITIZE_STRING);
-   $pass = sha1($_POST['pass']);
-   $pass = filter_var($pass, FILTER_SANITIZE_STRING);
-   $cpass = sha1($_POST['cpass']);
-   $cpass = filter_var($cpass, FILTER_SANITIZE_STRING);
+   $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+   $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+   $pass_raw = $_POST['pass'] ?? '';
+   $cpass_raw = $_POST['cpass'] ?? '';
+   $pass = sha1($pass_raw);
+   $cpass = sha1($cpass_raw);
 
    $select_user = $conn->prepare("SELECT * FROM `users` WHERE email = ?");
-   $select_user->execute([$email,]);
+   $select_user->execute([$email]);
    $row = $select_user->fetch(PDO::FETCH_ASSOC);
 
    if($select_user->rowCount() > 0){
       $message[] = 'email already exists!';
    }else{
-      if($pass != $cpass){
+      if($pass !== $cpass){
          $message[] = 'confirm password not matched!';
       }else{
          $insert_user = $conn->prepare("INSERT INTO `users`(name, email, password) VALUES(?,?,?)");
@@ -74,18 +69,6 @@ if(isset($_POST['submit'])){
    </form>
 
 </section>
-
-
-
-
-
-
-
-
-
-
-
-
 
 <?php include 'components/footer.php'; ?>
 
