08ba883976a2a33487aaef2c67fe2df81b3168f4
Subiramaniam Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-08-09 23:45:35 +0530

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,139 +1,84 @@
 <?php
-//session_start();
-include('login_check.php');
+// Make sure session is started and login checked outside this snippet
+// include('login_check.php');
 
-$user_id = $_SESSION['customer_id'];
+$user_id = $_SESSION['customer_id'] ?? null;
 
+// Connect once to hardware shop DB
+$conn = new mysqli("localhost", "root", "", "kn_raam_hardware");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-$conn = mysqli_connect("localhost","root","","mobile_shop");
+// Fetch user info if logged in
+$name = '';
+if ($user_id) {
+    $stmt = $conn->prepare("SELECT name FROM users WHERE user_id = ?");
+    $stmt->bind_param("i", $user_id);
+    $stmt->execute();
+    $stmt->bind_result($name);
+    $stmt->fetch();
+    $stmt->close();
+}
 
-
-$sql ="SELECT * FROM users WHERE user_id=$user_id";
-
-$res = mysqli_query($conn, $sql);
-
-if($res==true)
-{
-
-  $count_rows= mysqli_num_rows($res);
-  $sn=1;
-  //whether there is any data in database or not
-  if($count_rows>0)
-  {
-         while($row=mysqli_fetch_assoc($res))
-         {
-            $user_id=$row['user_id'];
-            $name=$row['name'];
-            $mail=$row['mail'];
-            $contact_no=$row['contact_no'];
-            $gender=$row['gender'];
-            $username=$row['username'];
-            $user_type=$row['user_type'];
-         }
-    
-      }    
-    }
-
+// Count cart items
+$cart_count = 0;
+if ($user_id) {
+    $stmt = $conn->prepare("SELECT COUNT(*) FROM cart WHERE user_id = ?");
+    $stmt->bind_param("i", $user_id);
+    $stmt->execute();
+    $stmt->bind_result($cart_count);
+    $stmt->fetch();
+    $stmt->close();
+}
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/styles.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<style>
-  .messages{
-    height:400px;
-  }
-</style>
-    <title>Mobile store</title>
-</head>
-<body>
 <nav class="navbar navbar-expand-lg bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
   <div class="container-fluid">
-    <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">Thikal Electronics</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">K.N. Raam Hardware</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" 
+            aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+    
     <div class="collapse navbar-collapse" id="navbarScroll">
       <ul class="navbar-nav ms-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+        
         <li class="nav-item">
-          <a class="nav-link   me-2" aria-current="page" href="index.php">Home</a>
+          <a class="nav-link me-2" href="index.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link me-2" href="products.php">Our products</a>
+          <a class="nav-link me-2" href="products.php">Our Products</a>
         </li>
-
         <li class="nav-item">
-          <a class="nav-link  me-2" href="brands.php">Brands</a>
+          <a class="nav-link me-2" href="brands.php">Brands</a>
         </li>
-
         <li class="nav-item">
-          <a class="nav-link me-2"    href="contactUs.php">Contact Us</a>
+          <a class="nav-link me-2" href="contactUs.php">Contact Us</a>
         </li>
-
         <li class="nav-item">
           <a class="nav-link me-2" href="about.php">About</a>
         </li>
-        <?php
-            if(!isset($_SESSION['customer_id'])){
-                ?>
-                    
-                            <li class="nav-item">
-                                <a class="nav-link me-2" href="login.php">Login</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link me-2" href="register.php">Sign up</a>
-                            </li>
-                           
-                        
-                <?php
-            }
-            else{
-            ?>
-                            <li class="nav-item">
-                              <?php
-                              $conn= mysqli_connect('localhost','root','') or die(mysqli_error());
 
-        
-                              //select db
-                               $db_select = mysqli_select_db($conn,'mobile_shop') or die(mysqli_error());
-                            
-                               $qry="SELECT * FROM cart WHERE user_id=$user_id";
-                              
-                               $result =mysqli_query($conn, $qry);
-                               if($result==true){
-                                  
-                                $count_rows= mysqli_num_rows($result);
-                                ?>
-                                  <a class="nav-link me-2" href="cart.php">Cart(<?php echo $count_rows;?>)</a>
-                                <?php
-                               }
-                              ?>
-                                
-                            </li>
-                        <li class="nav-item">
-                                <a class="nav-link me-2" href="my_profile.php"><?php echo $name;?></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link me-2" href="logout.php">Logout</a>
-                            </li>
-                           
-                      
-                <?php
-            }
-
-        ?>
+        <?php if (!$user_id): ?>
+          <li class="nav-item">
+            <a class="nav-link me-2" href="login.php">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link me-2" href="register.php">Sign Up</a>
+          </li>
+        <?php else: ?>
+          <li class="nav-item">
+            <a class="nav-link me-2" href="cart.php">Cart(<?php echo $cart_count; ?>)</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link me-2" href="my_profile.php"><?php echo htmlspecialchars($name); ?></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link me-2" href="logout.php">Logout</a>
+          </li>
+        <?php endif; ?>
       </ul>
-      
     </div>
   </div>
 </nav>
-
-
-        
